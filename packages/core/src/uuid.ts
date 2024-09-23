b415e2d57f7463e530a5e9286d356116bc3c83d1
Simon <simon.victory@swyftx.com.au>
2024-09-23 17:17:48 +1000

    cleanup

--- packages/core/src/uuid.ts
+++ packages/core/src/uuid.ts
@@ -1,5 +1,5 @@
 export class Uuid {
-  public constructor(public readonly value: Buffer) {
+  public constructor(public readonly value: string) {
     if (value.length !== 16) {
       throw new Error("UUID must be 16 bytes");
     }
